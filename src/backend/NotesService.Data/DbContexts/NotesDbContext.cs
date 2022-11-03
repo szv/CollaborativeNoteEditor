7781d2e6949d61fb6fe6cebb47da22eb6f0b7c9d
Sebastian <sebastian@szvetecz.at>
2022-11-03 13:38:40 +0100

    Configured Cosmos and added model

---
+++ src/backend/NotesService.Data/DbContexts/NotesDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesService.Data.DbConfigurations;
+using NotesService.Shared.Models;
+
+namespace NotesService.Data.DbContexts;
+
+public class NotesDbContext : DbContext
+{
+    public DbSet<Note> Notes => Set<Note>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new NoteConfiguration());
+        modelBuilder.HasDefaultContainer("notes");
+    }
+}
