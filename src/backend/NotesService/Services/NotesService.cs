3f129de64d71bf20773fc280cc3289daba701f46
Sebastian <sebastian@szvetecz.at>
2022-11-03 14:11:31 +0100

    Added NoteService without repository pattern

---
+++ src/backend/NotesService/Services/NotesService.cs
@@ -0,0 +1,58 @@
+﻿using NotesService.Data.DbContexts;
+using NotesService.Shared.Exceptions;
+using NotesService.Shared.Models;
+
+namespace NotesService.Services;
+
+internal class NotesService
+{
+    private readonly NotesDbContext _dbContext;
+
+    public NotesService(NotesDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public IAsyncEnumerable<Note> GetAsync()
+    {
+        return _dbContext.Notes.AsAsyncEnumerable();
+    }
+
+    public ValueTask<Note?> GetAsync(Guid id)
+    {
+        return _dbContext.Notes.FindAsync(id);
+    }
+
+    public async ValueTask<Note> GetOrThrow(Guid id)
+    {
+        return await _dbContext.Notes.FindAsync(id) ?? throw new NotFoundException();
+    }
+
+    public Task CreateAsync(Note note)
+    {
+        _dbContext.Notes.Add(note);
+        return _dbContext.SaveChangesAsync();
+    }
+
+    public async Task CreateOrUpdateAsync(Guid id, Note note)
+    {
+        Note? existing = await GetAsync(id);
+
+        if (existing is not null)
+        {
+            note.Id = id;
+            _dbContext.Notes.Update(note);
+        }
+        else
+            await CreateAsync(note);
+
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        Note note = await GetAsync(id) ?? throw new NotFoundException();
+        _dbContext.Notes.Remove(note);
+        await _dbContext.SaveChangesAsync();
+    }
+}
