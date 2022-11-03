195bdd76b34c5534b15adb5221adfd7ca5c62eba
Sebastian <sebastian@szvetecz.at>
2022-11-03 14:59:14 +0100

    Added dtos

---
+++ src/backend/NotesService.Shared/Transfer/NoteDtos.cs
@@ -0,0 +1,7 @@
+﻿namespace NotesService.Shared.Transfer;
+
+public record class NoteResponseDto(Guid Id, string Content, DateTime CreatedAt, DateTime UpdatedAt);
+
+public record class CreateNoteRequestDto(string Content);
+
+public record class UpdateNoteRequestDto(string Content);
