8364bd4b20d7ced4b339d8919906fcfd95c0fce6
Sebastian <sebastian@szvetecz.at>
2022-11-04 19:05:54 +0100

    Disabled https-redirection

--- src/backend/NotesService/Program.cs
+++ src/backend/NotesService/Program.cs
@@ -57,7 +57,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseCors();
 app.UseMiddleware<ExceptionMiddleware>();
 
