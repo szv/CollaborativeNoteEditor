1e595e507cb56d5d27b6849850d1851b9b5b4f4f
Sebastian <sebastian@szvetecz.at>
2022-11-08 13:31:00 +0100

    Added Milkdown as richtext-markdown-editor to frontend

--- src/frontend/collaborative-note-editor/src/app/editor/components/editor.component.ts
+++ src/frontend/collaborative-note-editor/src/app/editor/components/editor.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { defaultValueCtx, Editor, rootCtx } from '@milkdown/core';
+import { listener, listenerCtx } from '@milkdown/plugin-listener';
+import { replaceAll } from '@milkdown/utils';
+import { commonmark } from '@milkdown/preset-commonmark';
+import { gfm } from '@milkdown/preset-gfm';
+import { getNord } from '@milkdown/theme-nord';
+import { menu } from '@milkdown/plugin-menu';
 import { ControlValueAccessor } from '@ngneat/reactive-forms';
 
 @Component({
@@ -13,6 +20,11 @@ import { ControlValueAccessor } from '@ngneat/reactive-forms';
 export class EditorComponent extends ControlValueAccessor<string> implements OnInit {
   
   private _value: string | null = null;
+
+  private _setEditorValue?: (value: any) => void;
+
+  @ViewChild('editorRef') public editorRef!: ElementRef;
+  public defaultValue = '# Enter Markdown';
 
   constructor() {
     super();
@@ -29,7 +41,27 @@ export class EditorComponent extends ControlValueAccessor<string> implements OnI
   ngOnInit(): void {
   }
 
+  async ngAfterViewInit() {
+    const editor = await Editor.make()
+      .config((ctx) => {
+          ctx.set(rootCtx, this.editorRef!.nativeElement);
+          ctx.set(defaultValueCtx, this.defaultValue);
+          ctx.get(listenerCtx).markdownUpdated((ctx, markdown, prevMarkdown) => {
+            this.value = markdown;
+        });
+      })
+      .use(listener)
+      .use(commonmark)
+      .use(gfm)
+      .use(getNord(false))  // .use(nord) equals .use(getNord(true))
+      .use(menu)
+      .create();
+
+    this._setEditorValue = (value: any) => editor.action(replaceAll(value));
+  }
+
   writeValue(value: string): void {
     this.value = value;
+    setTimeout(() => this._setEditorValue?.(value), 0);
   }
 }
